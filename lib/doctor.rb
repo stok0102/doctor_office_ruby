aac7cc39943758b26e60bb5e5fc9cccd456808bb
Caleb Stokka and Noah Ramey <caleb+noah@>
2016-07-12 10:06:53 -0700

    three passing specs on doctor class

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -6,4 +6,16 @@ class Doctor
     @specialty = attributes.fetch(:specialty)
     @id = attributes[id]
   end
+
+  define_singleton_method(:all) do
+    returned_doctors = DB.exec("SELECT * FROM doctors;")
+    doctors = []
+    returned_doctors.each() do |doctor|
+      name = doctor.fetch("name")
+      specialty = doctor.fetch("specialty")
+      id = doctor.fetch("id").to_i()
+      doctors.push(Doctor.new({:name => name, :specialty => specialty, :id => id}))
+    end
+    doctors
+  end
 end
